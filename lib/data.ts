e0206e85686663f1a1d9a8fd3a44e9590e78322a
Shoib007 <skhantanda@gmail.com>
2023-10-04 21:04:39 +0530

    First Commit

--- lib/data.ts
+++ lib/data.ts
@@ -1,10 +1,12 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
+import { RiRobotLine } from "react-icons/ri";
+import { LiaChalkboardTeacherSolid } from "react-icons/lia";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import NetFlix from "@/public/netflix.png";
+import Keyloger from "@/public/project-1.jpg";
+import Trainer from "@/public/project-2.png";
 
 export const links = [
   {
@@ -35,73 +37,71 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Robotics Engineer",
+    location: "Bangalore, KR",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
+      "I develop Robotics Activities and Coding for Companies B2B Clients and give then training on it.",
+    icon: React.createElement(RiRobotLine),
+    date: "2022 - Current",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "AI/ML Lecturer",
+    location: "Ambedkar Nagar, UP",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
+      "I impacted 100+ students life by teaching Computer Science, Coding and Artificial Intelligence & Machine Learning",
+    icon: React.createElement(LiaChalkboardTeacherSolid),
     date: "2019 - 2021",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "Jr. Faculty of Hardware & Network",
+    location: "Bangalore, KA",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
+      "I impacted 20+ students life by train them about Computer Hardware & Networks",
+    icon: React.createElement(LiaChalkboardTeacherSolid),
+    date: "2018 - 2019",
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Netflix Clone",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      "I created this project to learn Tailwind with Vite.js",
+    tags: ["React", "Vite.js", "Django", "Tailwind", "Docker"],
+    imageUrl: NetFlix,
   },
   {
-    title: "rmtDev",
+    title: "Trainer Management System", 
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      "I developed this project to manage the training + Trainers in my company",
+    tags: ["React", "JavaScript", "Bootstrap", "ContextAPI", "Docker"],
+    imageUrl: Trainer,
   },
   {
-    title: "Word Analytics",
+    title: "Python Keylogger",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "I created this Keylogger when I was teacher my students about Ethical Hacking",
+    tags: ["Python", "Socket", "SMTP", "Threading"],
+    imageUrl: Keyloger,
   },
 ] as const;
 
 export const skillsData = [
-  "HTML",
-  "CSS",
+  "HTML5",
+  "CSS3",
   "JavaScript",
   "TypeScript",
+  "AWS",
   "React",
   "Next.js",
   "Node.js",
   "Git",
   "Tailwind",
-  "Prisma",
   "MongoDB",
   "Redux",
-  "GraphQL",
-  "Apollo",
-  "Express",
   "PostgreSQL",
   "Python",
   "Django",
   "Framer Motion",
+  "Bootstrap",
 ] as const;
