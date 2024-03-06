4f458849eed649e18d961b6726c62163cc125ea1
Mohd Shoib <skhantanda@gmail.com>
2024-03-06 10:52:37 +0530

    Added underline underline techs in Intro section

--- components/intro.tsx
+++ components/intro.tsx
@@ -61,7 +61,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Shoib.</span> I'm a <span className="font-bold">Backend developer</span> with<span className="font-bold">2 years</span> of experience. I enjoy building <span className="italic">Web apps</span>. My focus is <span className="underline">React (Vite & Next.js) + Django and learning Other technologies like Springboot, Node.js, etc</span>.
+        <span className="font-bold">Hello, I'm Shoib.</span> I'm a <span className="font-bold">Backend developer</span> with<span className="font-bold">2 years</span> of experience. I enjoy building <span className="italic">Web apps</span>. My focus is <span className="underline">React (Vite & Next.js) + Django</span>  and learning Other technologies like <span className="underline">Springboot, Node.js, etc</span>.
       </motion.h1>
 
       <motion.div
