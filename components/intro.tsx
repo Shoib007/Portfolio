2f5658142528d18babd6e2cf2ad7a008809163c4
Mohd Shoib <skhantanda@gmail.com>
2024-09-15 19:45:02 +0530

    Modified Intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -61,7 +61,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Shoib.</span> I'm a <span className="font-bold">Backend developer</span> with<span className="font-bold">2 years</span> of experience. I enjoy building <span className="italic">Web apps</span>. My focus is <span className="underline">React (Vite & Next.js) + Django</span>  and learning Other technologies like <span className="underline">Springboot, Node.js, etc</span>.
+        <span className="font-bold">Hello, I'm Shoib.</span> I'm a <span className="font-bold">Software Engineer</span> with<span className="font-bold"> 2 years</span> of experience. I enjoy building <span className="italic">Web apps, working on Robotics and Electronics</span>. My focus is <span className="underline">on Backend Development and Robotics as Hobby</span>  and learning Other technologies like <span className="underline">Go Lang, Node.js, etc</span>.
       </motion.h1>
 
       <motion.div
